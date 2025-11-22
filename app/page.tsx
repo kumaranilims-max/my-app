9bad8aed82136cf0e16c91d017920fae5f567cb9
Anil <kumaranil.imsL@gmail.com>
2025-11-22 16:19:38 +0530

    update again

--- app/page.tsx
+++ app/page.tsx
@@ -235,6 +235,7 @@ export default function Home() {
                                 >
                                   +
                                 </button>
+                                
                               </div>
                               <button 
                                 onClick={() => handleRemoveFromCart(item.product.id)} 
